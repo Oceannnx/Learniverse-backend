9f1a69c595fe6fc7b9ed8cc8e58225893bea9bb7
หนมจีบ <onopingping@gmail.com>
2024-06-07 03:12:51 +0700

    ✨ feat(server): adding exam model

---
+++ src/model/exam.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const examSchema = new mongoose.Schema({
+  label: {
+    type: "String",
+  },
+  grade: {
+    type: "Date",
+  },
+  subject: {
+    type: "String",
+  },
+  level: {
+    type: "String",
+  },
+  totalmark: {
+    type: "String",
+  },
+  duration: {
+    type: "String",
+  },
+  createBy: {
+    type: "String",
+  },
+  content: {
+    type: ["Mixed"],
+  },
+});
+
+export const examModel = mongoose.model("exams", examSchema);
